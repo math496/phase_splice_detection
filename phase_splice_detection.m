806adaeb72cc444a6d30ddd3b3bdbd49f60f2826
Efron Licht <Efron_Licht@yahoo.com>
2016-03-17 14:56:00 -0700

    initial commit

---
+++ phase_splice_detection.m
@@ -0,0 +1,21 @@
+function imgOut = phase_splice_detection(imgIn);
+
+%% 0: INPTUT HANDLING 
+
+%% 1: FIND PREDICTED ERROR
+
+%% 2: TAKE WAVELET DECOMPOSITIONS
+
+% wavelet decomposition of image
+
+% wavelet decomposition of predicted_error
+
+%% 3. HISTORGRAMS
+
+% histogram of image
+
+% histogram of wavelet
+
+% histeogram of predicted_error
+
+%% 4. Fourier Transforms
